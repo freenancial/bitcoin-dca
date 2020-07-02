54580e789c87fd6d6becc263cc25c544012287bd
freenancial <freenancial@protonmail.com>
2020-07-01 22:11:11 -0700

    Add an empty line after withdraw log

--- coinbase_pro.py
+++ coinbase_pro.py
@@ -66,6 +66,7 @@ class CoinbasePro:
     print(f"Withdrawing ${amount} Bitcoin to address {address} ...")
     withdraw_result = self.auth_client.crypto_withdraw(amount, 'BTC', address)
     print(withdraw_result)
+    print()
 
   def getBitcoinWorth(self):
     return self.getBitcoinBalance() * self.getBitcoinPrice()
