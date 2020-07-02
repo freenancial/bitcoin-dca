2a3819c4bf9c8217e2afa08d30a5dd1b794cdaa6
freenancial <freenancial@protonmail.com>
2020-07-01 17:24:26 -0700

    Add `install.sh` and `bitcoin_dca.sh` to make this tool easier to use

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bitcoin-dca
+
+Buy bitcoin every hour on CoinbasePro.
+
+To install this tool:
+`./install.sh`
+
+To use this tool:
+`./bitcoin_dca.sh`
---
+++ bitcoin_dca.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Your Coinbase Pro API key:"
+read -s API_KEY
+export API_KEY
+
+echo "Your Coinbase Pro API secret:"
+read -s API_SECRET
+export API_SECRET
+
+echo "Your Coinbase Pro API passphrase:"
+read -s PASSPHRASE
+export PASSPHRASE
+
+./buy_bitcoin.py
