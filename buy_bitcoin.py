8c4ee148dd86fdb5b7a54184630cf771ee4e5827
freenancial <freenancial@protonmail.com>
2020-07-12 12:05:22 -0700

    Withdraw BTC base on buy history (#8)

--- buy_bitcoin.py
+++ buy_bitcoin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from config import DCA_USD_AMOUNT, DCA_FREQUENCY
-from config import AUTO_WITHDRAWL, WITHDRAW_THRESHOLD, MASTER_PUBLIC_KEY, BEGINNING_ADDRESS
+from config import AUTO_WITHDRAWL, WITHDRAW_EVERY_X_BUY, MASTER_PUBLIC_KEY, BEGINNING_ADDRESS
 
 from coinbase_pro import CoinbasePro
 from address_selector import AddressSelector
@@ -14,29 +14,35 @@ API_SECRET = os.environ['API_SECRET']
 PASSPHRASE = os.environ['PASSPHRASE']
 address_selector = AddressSelector(MASTER_PUBLIC_KEY, BEGINNING_ADDRESS)
 
+next_buy_datetime = datetime.datetime.now() + datetime.timedelta(0, DCA_FREQUENCY)
+
 while True:
   print('--------------------------------------------------')
   coinbase_pro = CoinbasePro(API_KEY, API_SECRET, PASSPHRASE)
+  # Buy Bitcoin
   try:
-    coinbase_pro.refreshBalance()
+    coinbase_pro.showBalance()
+    coinbase_pro.buyBitcoin(DCA_USD_AMOUNT)
   except Exception as e:
-    print(f"Refresh balance failed, error: {str(e)}")
+    print(f"Error: {str(e)}")
     print("Waiting for 60 seconds to retry ...")
     print()
     time.sleep(60)
     continue
   
-  coinbase_pro.showBalance()
-  coinbase_pro.depositUSDCFromCoinbase(DCA_USD_AMOUNT)
-  coinbase_pro.convertUSDCToUSD(DCA_USD_AMOUNT)
-  coinbase_pro.buyBitcoin(DCA_USD_AMOUNT)
-  coinbase_pro.showBalance()
+  # Show the new balance and withdraw BTC if needed
+  try:
+    coinbase_pro.showBalance()
 
-  if AUTO_WITHDRAWL and coinbase_pro.getBitcoinWorth() >= WITHDRAW_THRESHOLD and address_selector.getWithdrawAddress() :
-    coinbase_pro.withdrawBitcoin(coinbase_pro.getBitcoinBalance(), address_selector.getWithdrawAddress())
-    address_selector.incrementAddressIndex()
+    if AUTO_WITHDRAWL and coinbase_pro.getRecentBuysCount() >= WITHDRAW_EVERY_X_BUY:
+      coinbase_pro.withdrawBitcoin(coinbase_pro.getBitcoinBalance(), address_selector.getWithdrawAddress())
+      address_selector.incrementAddressIndex()
+  except Exception as e:
+    print(f"Error: {str(e)}")
 
-  next_buy_datetime = datetime.datetime.now() + datetime.timedelta(0, DCA_FREQUENCY)
+  # Wait for next buy time
   print(f"Waiting until {next_buy_datetime} to buy ${DCA_USD_AMOUNT} Bitcoin...")
   print()
-  time.sleep(DCA_FREQUENCY)
+  while datetime.datetime.now() < next_buy_datetime:
+    time.sleep(1)
+  next_buy_datetime = datetime.datetime.now() + datetime.timedelta(0, DCA_FREQUENCY)
--- coinbase_pro.py
+++ coinbase_pro.py
@@ -1,11 +1,13 @@
 import cbpro
 import time
+import itertools
+import math
 
 class CoinbasePro:
   def __init__(self, api_key, api_secret, passphrase):
     self.auth_client = cbpro.AuthenticatedClient(api_key, api_secret, passphrase)
 
-  def refreshBalance(self):
+  def refresh(self):
     self.accounts = self.auth_client.get_accounts()
     self.coinbase_accounts = self.auth_client.get_coinbase_accounts()
 
@@ -13,6 +15,7 @@ class CoinbasePro:
     self.usd_account = self.getAccount('USD')
     self.btc_account = self.getAccount('BTC')
     self.coinbase_usdc_account = self.getCoinbaseAccount('USDC')
+    time.sleep(1)
 
   def getAccount(self, currency):
     try:
@@ -31,29 +34,60 @@ class CoinbasePro:
       raise e
 
   def showBalance(self):
+    self.refresh()
     print()
-    print("Coinbase USDC balance: {:.2f}".format(float(self.coinbase_usdc_account['balance'])))
-    print("BTC balance: {}".format(float(self.btc_account['balance'])))
+    print("Coinbase USDC balance: ${:.2f}".format(float(self.coinbase_usdc_account['balance'])))
+    print("USDC balance: ${:.2f}".format(self.usdc_balance()))
+    print("USD balance: ${:.2f}".format(self.usd_balance()))
+    print("BTC balance: ₿{}".format(float(self.btc_account['balance'])))
     print()
+
+  def getRecentBuysCount(self):
+    btc_account_history = self.auth_client.get_account_history(self.btc_account['id'])
+    return sum(1 for _ in itertools.takewhile(lambda x: x['type'] == 'match', btc_account_history))
 
   def depositUSDCFromCoinbase(self, amount):
+    self.refresh()
+
+    amount = math.ceil(amount * 100) / 100
+    print(f"Depositing ${amount} USDC from Coinbase ...")
     self.auth_client.coinbase_deposit(amount, 'USDC', self.coinbase_usdc_account['id'])
-    self.refreshBalance()
+    time.sleep(5)
+    print('  Done')
 
   def convertUSDCToUSD(self, amount):
+    self.refresh()
+
+    amount = math.ceil(amount * 100) / 100
+    if self.usdc_balance() < amount:
+      self.depositUSDCFromCoinbase(amount - self.usdc_balance())
+
+    print(f"Converting ${amount} USDC to USD ...")
     self.auth_client.convert_stablecoin(amount, 'USDC', 'USD')
-    self.refreshBalance()
+    time.sleep(5)
+    print('  Done')
 
   def buyBitcoin(self, usd_amount):
-    print(f"Buying ${usd_amount} Bitcoin ...")
+    self.refresh()
 
+    usd_amount = math.ceil(usd_amount * 100) / 100
+    if self.usd_balance() < usd_amount:
+      self.convertUSDCToUSD(usd_amount - self.usd_balance())
+
+    print(f"Buying ${usd_amount} Bitcoin ...")
     product_id = 'BTC-USD'
     order_result = self.auth_client.place_market_order(product_id, 'buy', funds=usd_amount)
     while not order_result['settled']:
       time.sleep(1)
       order_result = self.auth_client.get_order(order_result['id'])
     self.printOrderResult(order_result)
-    self.refreshBalance()
+    time.sleep(5)
+
+  def usdc_balance(self):
+    return float(self.usdc_account['balance'])
+
+  def usd_balance(self):
+    return float(self.usd_account['balance'])
 
   def printOrderResult(self, order_result):
     print(f"  Size: \t{ round( float(order_result['specified_funds']), 2 )}")
@@ -63,7 +97,7 @@ class CoinbasePro:
     print(f"  Date: \t{order_result['done_at']}")
 
   def withdrawBitcoin(self, amount, address):
-    print(f"Withdrawing ${amount} Bitcoin to address {address} ...")
+    print(f"Withdrawing ₿{amount} Bitcoin to address {address} ...")
     withdraw_result = self.auth_client.crypto_withdraw(amount, 'BTC', address)
     print(withdraw_result)
     print()
