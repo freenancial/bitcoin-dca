dff0e6723580b303b8937dfe553073d6f56c5b75
freenancial <freenancial@protonmail.com>
2020-08-09 15:06:19 -0700

    Add indent

--- bitcoin_dca/coinbase_pro.py
+++ bitcoin_dca/coinbase_pro.py
@@ -86,7 +86,7 @@ class CoinbasePro:
         amount = math.ceil(amount * 100) / 100
         Logger.info(f"Depositing ${amount} USDC from Coinbase ...")
         result = self.auth_client.coinbase_deposit(amount, "USDC", self.coinbaseUSDCAccount().id)
-        Logger.info(result)
+        Logger.info(f"  {result}")
         time.sleep(5)
 
     def convertUSDCToUSD(self, amount):
@@ -98,7 +98,7 @@ class CoinbasePro:
 
         Logger.info(f"Converting ${amount} USDC to USD ...")
         result = self.auth_client.convert_stablecoin(amount, "USDC", "USD")
-        Logger.info(result)
+        Logger.info(f"  {result}")
         time.sleep(5)
 
     def buyBitcoin(self, usd_amount):
