1195d3110aeb1a4e4783bc29c07f8037bef8ab91
freenancial <freenancial@protonmail.com>
2020-12-29 11:43:47 -0800

    Refine logging

--- bitcoin_dca/bitcoin_dca.py
+++ bitcoin_dca/bitcoin_dca.py
@@ -44,14 +44,15 @@ class BitcoinDCA:
         self.db_manager = DBManager()
         self.next_robinhood_buy_datetime = self.calcRobinhoodFirstBuyTime()
         if is_coinbase:
+            Logger.info("\n")
+            Logger.info("----------------------")
+            Logger.info("----------------------")
+            Logger.info("Coinbase DCA started")
+            Logger.info("")
             self.coinbase_pro = self.newCoinbaseProClient()
             self.next_buy_datetime = self.calcFirstBuyTime()
 
     def newCoinbaseProClient(self):
-        Logger.info("----------------------")
-        Logger.info("----------------------")
-        Logger.info("Coinbase DCA started")
-
         return CoinbasePro(
             self.secrets["api_key"],
             self.secrets["api_secret"],
@@ -151,13 +152,12 @@ class BitcoinDCA:
         )
 
     def startCoinbaseDCA(self):
-        Logger.info("")
+        self.coinbase_pro.showBalance()
         Logger.info(
             f"We'll wait for {self.next_buy_datetime.strftime('%Y-%m-%d %H:%M:%S')} "
             f"to buy ${default_config.dca_usd_amount} Bitcoin on Coinbase..."
         )
         Logger.info("")
-        self.coinbase_pro.showBalance()
 
         while True:
             self.waitForNextBuyTime()
