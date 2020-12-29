8d7d972e008d8887d7343185e1f984251c1f76c0
freenancial <freenancial@protonmail.com>
2020-12-29 11:30:14 -0800

    Update logging order

--- bitcoin_dca/bitcoin_dca.py
+++ bitcoin_dca/bitcoin_dca.py
@@ -44,21 +44,13 @@ class BitcoinDCA:
         self.db_manager = DBManager()
         self.next_robinhood_buy_datetime = self.calcRobinhoodFirstBuyTime()
         if is_coinbase:
-            self.next_buy_datetime = self.calcFirstBuyTime()
             self.coinbase_pro = self.newCoinbaseProClient()
+            self.next_buy_datetime = self.calcFirstBuyTime()
 
     def newCoinbaseProClient(self):
         Logger.info("----------------------")
         Logger.info("----------------------")
         Logger.info("Coinbase DCA started")
-
-        Logger.info("")
-        Logger.info(
-            f"We'll wait for {self.next_buy_datetime.strftime('%Y-%m-%d %H:%M:%S')} "
-            f"to buy ${default_config.dca_usd_amount} Bitcoin on Coinbase..."
-        )
-        Logger.info("")
-        self.coinbase_pro.showBalance()
 
         return CoinbasePro(
             self.secrets["api_key"],
@@ -159,6 +151,14 @@ class BitcoinDCA:
         )
 
     def startCoinbaseDCA(self):
+        Logger.info("")
+        Logger.info(
+            f"We'll wait for {self.next_buy_datetime.strftime('%Y-%m-%d %H:%M:%S')} "
+            f"to buy ${default_config.dca_usd_amount} Bitcoin on Coinbase..."
+        )
+        Logger.info("")
+        self.coinbase_pro.showBalance()
+
         while True:
             self.waitForNextBuyTime()
 
